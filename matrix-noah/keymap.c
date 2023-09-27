13804ad45850b7340dbfd88abc29273bbefe9c82
qtkite <kite@uwu.icu>
2023-09-27 18:54:01 +1000

    updates

--- matrix-noah/keymap.c
+++ matrix-noah/keymap.c
@@ -10,11 +10,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,     KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U,    KC_I,   KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
         KC_LCTL,    KC_A,     KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT,  KC_ENT, KC_PGUP,
         KC_LSFT,    KC_Z,     KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,   KC_UP,  KC_PGDN,
-        KC_LCTL, KC_LGUI,  KC_LALT,                 KC_SPC,                 MO(1), KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT),
+        KC_LCTL, KC_LGUI,  KC_LALT,                 KC_SPC,                 MO(1), KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT),
     [1] = LAYOUT_default(
-        KC_GRAVE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, _______, QK_BOOT,
-          KC_CAPS, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2,  KC_MS_WH_UP,  KC_F14,  KC_F24, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_GRAVE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, _______, QK_RBT,
+          _______, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2,  KC_MS_WH_UP,  KC_F14,  KC_F24, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_BTN3, _______, RGB_TOG, RGB_MOD,   KC_UP,KC_RIGHT, _______, _______, _______, _______,
-        _______, KC_MS_BTN4, KC_MS_BTN5, KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______,                    _______,_______,          _______, _______, _______),
+        _______, KC_MS_BTN4, KC_MS_BTN5, KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, _______,
+        _______, _______, _______,                   _______,                    _______,_______,          _______, KC_VOLD, _______),
 };
