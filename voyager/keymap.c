5745550500945a9a525bad71d3b5e6e5ffe7706a
pgkt04 <23523441+pgkt04@users.noreply.github.com>
2025-04-01 22:15:29 +1100

    bottom is now mostly space

--- voyager/keymap.c
+++ voyager/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPC,
     KC_LCTL, KC_A,    KC_S,        KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RSFT, KC_ENTER),
     KC_LSFT, MT(MOD_LSFT, KC_Z),     KC_X,           KC_C,           KC_V,          KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH, LT(3, KC_GRAVE),
-                                                    MT(MOD_LGUI,KC_SPACE), MT(MOD_LALT, KC_BSPC),                                LT(2, KC_SPACE),LT(1,KC_ENTER)
+                                                    MT(MOD_LGUI,KC_SPACE), MT(MOD_LALT, KC_SPACE),                                LT(2, KC_SPACE),LT(1,KC_ENTER)
   ),
   [1] = LAYOUT_voyager(
     _______,      _______,          _______,          _______,          _______,          _______,                                          _______,          _______,          _______,       KC_MINUS,     KC_EQUAL,    KC_GRAVE,
@@ -188,3 +188,4 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
 };
+
