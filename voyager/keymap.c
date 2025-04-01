f98efb29482968be1362422d500f3238a3c9a2f4
pgkt04 <23523441+pgkt04@users.noreply.github.com>
2025-04-02 00:23:58 +1100

    rev

--- voyager/keymap.c
+++ voyager/keymap.c
@@ -19,14 +19,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     QK_GRAVE_ESCAPE,    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_TAB,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPC,
-    KC_LCTL, KC_A,    KC_S,        KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RSFT, KC_ENTER),
+    KC_LCTL, KC_A,    KC_S,        KC_D,           KC_F,           KC_G,                                                      KC_H,    KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RSFT, KC_QUOTE),
     KC_LSFT, MT(MOD_LSFT, KC_Z),     KC_X,           KC_C,           KC_V,          KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH, LT(3, KC_GRAVE),
-                                                    MT(MOD_LGUI,KC_SPACE), MT(MOD_LALT, KC_SPACE),                                LT(2, KC_SPACE),LT(1,KC_ENTER)
+                                                    MT(MOD_LGUI,KC_SPACE), MT(MOD_LALT, KC_SPACE),                                LT(2, KC_SPACE), LT(1,KC_ENTER)
   ),
   [1] = LAYOUT_voyager(
     _______,      _______,          _______,          _______,          _______,          _______,                                          _______,          _______,          _______,       KC_MINUS,     KC_EQUAL,    KC_GRAVE,
     _______, _______,        _______,          _______,        _______,         _______,                                        _______,   _______,           KC_UP,           KC_LBRC,       KC_RBRC,       KC_BSLS,
-    _______,        _______, _______,        KC_ASTR,        KC_LPRN,        KC_RPRN,                                        _______, KC_LEFT, KC_DOWN, KC_RIGHT,        _______,        KC_QUOTE,
+    _______,        _______, _______,        KC_ASTR,        KC_LPRN,        KC_RPRN,                                        _______, KC_LEFT, KC_DOWN, KC_RIGHT,        _______,       KC_ENTER ,
     _______, _______, KC_LBRC,        KC_RBRC,        KC_LCBR,        KC_RCBR,                                        KC_BSPC,        _______,         _______,           _______,         _______,       _______,
                                                     _______, _______,                                 _______, _______
   ),
