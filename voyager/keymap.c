044d0411681a7bfc4b65122838b0e5e91445be3f
pgkt04 <23523441+pgkt04@users.noreply.github.com>
2025-04-03 19:00:42 +1100

    ?

--- voyager/keymap.c
+++ voyager/keymap.c
@@ -186,6 +186,6 @@ void dance_1_reset(tap_dance_state_t *state, void *user_data) {
 
 tap_dance_action_t tap_dance_actions[] = {
         [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
-        [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
+        [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset)
 };
 
